4f0a684fada5cb3a4fdc308a7334846b5f9cf857
Jesus Mercado <anotherjez2@gmai.com>
2020-08-06 18:00:50 -0400

    quizas se arreglo

--- pages/panel/pagoconsulta.php
+++ pages/panel/pagoconsulta.php
@@ -38,24 +38,21 @@ if(!($user->getRole() == 3)){
 
 if($_POST){
 
-    foreach($_POST as &$value){
-        $value = addslashes($value);
-    }        
-    
-    extract($_POST);
-    $monto ="select costo from costoconsultas where id = 1";
+  foreach($_POST as &$value){
+      $value = addslashes($value);
+  }        
   
-    $sql = "select * from consultas where title = '{$title}'";
- $objs = Connection::query_arr($sql);
- $objs1 = Connection::query_arr($monto);
-    
-      /*  $sql = "insert into consultas(title, costo, monto_pagado) 
-        values('{$title}','{$nombre}','{$monto}')";*/
-  
-    
-    $rsid = Connection::execute($sql, true);
-    $rsid1 = Connection::execute($monto, true);
-    var_dump($monto);
+  extract($_POST);
+  $sql ="select costo from costoconsultas where id = 1";
+
+  $costo = Connection::query_arr($sql);
+  $costo = $costo[0];
+  $sql = "insert into consultas(title, costo, monto_pagado) 
+  values('{$title}','{$costo['costo']}','{$monto}')";
+
+  Connection::execute($sql);
+
+  header("Location:dashboard.php");
 }
 
 include('headerpanel.php');
@@ -64,7 +61,7 @@ include('headerpanel.php');
 
 <div class="container" style="padding-bottom: 40px;">
     
-    <h2>Añadir Paciente</h2>
+    <h2>Registrar pago de consulta</h2>
     <br>    
     <form enctype="multipart/form-data" method="POST">
     <?= Input('title','Titulo de la Consulta','', ['placeholder'=>'Ex: Consulta con el Dr.Perez por las sgtes razones.....']) ?>
@@ -77,14 +74,5 @@ include('headerpanel.php');
         <a href="dashboard.php" class="btn btn-secondary">Cancelar</a>
     </form>
 </div>
-<script>
-    $(document).ready(function(){
-        $('.pasaporte').mask('AA0000000');
-        $('.room').mask('000');
-        $('.telefono').mask('000000000000000');
-    });
-</script>
-<script src="../../assets/js/jquery.mask.min.js"></script>
-<script src="../../assets/js/guests.js"></script>
 
 <?php include('../footer.php'); ?>
